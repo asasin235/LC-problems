8f57b87bca3cb9697eac18e3088c1d314ba8297e
asasin235 <42548259+asasin235@users.noreply.github.com>
2021-07-08 22:30:18 +0530

    Time: 496 ms (5.13%), Space: 16.8 MB (59.98%) - LeetHub

---
+++ subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        running_sum = 0
+        hash_table = collections.defaultdict(lambda:0)
+        total = 0
+        for x in nums:
+            running_sum += x
+            sum = running_sum - k
+            if sum in hash_table:
+                total += hash_table[sum]
+            if running_sum == k:
+                total += 1
+            hash_table[running_sum] += 1
+        return total
+        
