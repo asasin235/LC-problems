9abdd8967c0a121277573b10b4b659e9593ed887
asasin235 <42548259+asasin235@users.noreply.github.com>
2021-10-13 12:13:57 +0530

    Time: 20 ms (99.34%), Space: 14.3 MB (8.08%) - LeetHub

---
+++ fibonacci-number/fibonacci-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        a=0
+        b=1
+        c=0
+        if(n==0 or n==1):
+            return n
+        for i in range(2,n+1):
+            c=a+b
+            a=b
+            b=c
+        return c
+        
+        
+        
