cfb424a1b59260ae9dab3a00f77dacb0c7c4dd6f
Aatif Rashid <42548259+asasin235@users.noreply.github.com>
2022-01-15 17:31:00 +0530

    Time: 132 ms (29.98%), Space: 49.1 MB (13.68%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<int> dp(amount+1,INT_MAX-1);
-        dp[0]=0;// basically no if no money needed no coins needed.
-        for(int i=1;i<=amount;i++){
-            for(int j=1;j<=n;j++){
-                if(i-coins[j-1]>=0){
-                    // take the coin if it gives better ans
-                    dp[i]=min(dp[i],dp[i-coins[j-1]]+1);
-                }else{
-                    dp[i]=dp[i];// dont take the coin
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,INT_MAX-1000));
+        dp[0][0]=0;
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(j>=coins[i-1]){
+                    dp[i][j]=min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
                 }
+                else dp[i][j]=dp[i-1][j];
             }
         }
-        if(dp[amount]==INT_MAX-1) return -1;
-        else return dp[amount];
+        return dp[n][amount]==INT_MAX-1000?-1:dp[n][amount];
     }
     
 };
