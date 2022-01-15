f3ef634270261245a97f4dfef9b0679460a92694
Aatif Rashid <42548259+asasin235@users.noreply.github.com>
2022-01-15 18:41:34 +0530

    Time: 136 ms (28.34%), Space: 49.2 MB (11.01%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    
     int helper(int idx,vector<int> &coins,int amount,vector<vector<int>> &dp){
         if(amount==0)
             return 0;
@@ -16,15 +15,35 @@ public:
         }else{
             return dp[idx][amount]= helper(idx-1,coins,amount,dp);
         }
-        
-        //return -1;
-        
-        
+         
     }
+    
+    
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n+1,vector<int> (amount+1,-1));
-        int x=helper(n,coins,amount,dp);
-        return x==INT_MAX-100?-1:x;
+        vector<vector<int>> dp(n+1,vector<int> (amount+1));
+        dp[0][0]=0;
+        for(int i=1;i<=amount;i++){
+            dp[0][i]=INT_MAX-100;
+        }
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(coins[i-1]>j){
+                    // not take
+                    dp[i][j]=dp[i-1][j];
+                }else{
+                    dp[i][j]=min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
+                }
+            }
+        }
+        if(dp[n][amount]==INT_MAX-100){
+            return -1;
+        }
+       return dp[n][amount];
+        // int x=helper(n,coins,amount,dp);
+        // return x==INT_MAX-100?-1:x;
     }
 };
